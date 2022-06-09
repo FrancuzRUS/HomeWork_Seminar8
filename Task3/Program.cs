54afa8e92717d3fb481fcc13df4d60fede860329
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-06-09 09:55:24 +0300

    Task3 and Task4 Completed

---
+++ Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// 3. Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента
+Console.WriteLine("Введите количество строк в массиве: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите глубину массива: ");
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine("----------------------------------------");
+
+int[,,] MASS3D = new int[a, b, c];
+int index = 20;
+
+for (int i = 0; i < MASS3D.GetLength(0); i++)
+{
+    for (int j = 0; j < MASS3D.GetLength(1); j++)
+    {
+        for (int k = 0; k < MASS3D.GetLength(2); k++)
+        {
+            MASS3D[i, j, k] = index;
+            Console.WriteLine($" Элемент массива {MASS3D[i,j,k]} имеет индексы i = {i}, j = {j}, k = {k} ");
+            index++;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿// 4. Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
+
+Console.WriteLine("Введите n для треугольника Паскаля");
+int n = int.Parse(Console.ReadLine());
+int space=0;
+ 
+int Factorial (int n)
+{
+    if (n==0 || n==1 ) return 1;
+    else return n*Factorial(n-1);
+}
+
+for (int i =0; i<=n; i++)
+{ Console.WriteLine();
+    for (space=((n-i)*2); space>0; space--)
+        {
+            Console.Write(" "); 
+        }
+    
+    for (int j =0; j<i+1; j++)
+    {
+        
+         Console.Write($" {Factorial(i)/(Factorial(j)*Factorial(i-j))} ");
+    }
+}
+
+
+
+
